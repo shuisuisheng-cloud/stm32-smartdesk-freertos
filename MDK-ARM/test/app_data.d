3a4040ada14dde4ef4c643fd2ec0a38b1aa278d1
shisuisheng-cloud <shuisuisheng@gmail.com>
2026-05-26 15:59:39 +0800

    chore: stop tracking keil build outputs

--- MDK-ARM/test/app_data.d
+++
@@ -1,3 +0,0 @@
-test\app_data.o: ..\User\App\app_data.c
-test\app_data.o: ..\User\App\app_data.h
-test\app_data.o: F:\qianrushi\ARM\ARMCC\Bin\..\include\stdint.h
--- MDK-ARM/test/ssd1306_fonts.d
+++
@@ -1,5 +0,0 @@
-test\ssd1306_fonts.o: ..\User\OLED\ssd1306_fonts.c
-test\ssd1306_fonts.o: ..\User\OLED\ssd1306_fonts.h
-test\ssd1306_fonts.o: F:\qianrushi\ARM\ARMCC\Bin\..\include\stdint.h
-test\ssd1306_fonts.o: ..\User\OLED\ssd1306_conf.h
-test\ssd1306_fonts.o: F:\qianrushi\ARM\ARMCC\Bin\..\include\stddef.h
