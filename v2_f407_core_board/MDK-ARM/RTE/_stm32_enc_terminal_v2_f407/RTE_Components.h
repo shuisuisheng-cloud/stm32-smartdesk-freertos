412843476394de84022cff4efd3872ab59ffa8a2
shisuisheng-cloud <shuisuisheng@gmail.com>
2026-07-02 22:03:20 +0800

    feat: add STM32F407 V2 base project and USART2 debug

---
+++ v2_f407_core_board/MDK-ARM/RTE/_stm32_enc_terminal_v2_f407/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'stm32_enc_terminal_v2_f407' 
+ * Target:  'stm32_enc_terminal_v2_f407' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
